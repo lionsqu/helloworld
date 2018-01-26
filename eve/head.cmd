cc3690786f185132ac13c78732d43fd89c671f25
lionsqu <lionsqu@yahoo.com>
2018-01-27 06:28:13 +0800

    补提交头文件

---
+++ eve/head.cmd
@@ -0,0 +1,17 @@
+function form_paras(){
+	echo
+}
+
+function version_gt() { 
+	test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; 
+}
+function version_le() { 
+	test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; 
+}
+function version_lt() { 
+	test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; 
+}
+function version_ge() { 
+	test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; 
+}
+
