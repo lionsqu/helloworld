437a68ba1e0874e4cf482bc4ce61856c261f73a6
lionsqu <lionsqu@yahoo.com>
2018-02-01 03:13:50 +0800

    添加Dialog代码生成器测试代码

--- eve/test.sh
+++ eve/test.sh
@@ -2,9 +2,22 @@ folder=$(cd "$(dirname "$0")";pwd)
 source $folder/head.cmd
 
 
-
-python test.py create
-
-
 result=$?
 echo "result="$result
+
+
+
+function dialog_calendar() {
+        echo 'dialog --title "title" --backtitle "backtitle" --calendar "date" 1 1'  
+        return
+}
+
+
+$(dialog_calendar)
+dialog_calendar > code.txt
+
+
+
+
+
+
