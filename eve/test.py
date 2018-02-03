4d3ce989dc7ef831cd930ff44ccd0b1c9a6b05ee
lionsqu <lionsqu@yahoo.com>
2018-02-03 22:57:26 +0800

    添加python的配置文件读写多行数据的测试代码

--- eve/test.py
+++ eve/test.py
@@ -1,25 +1,6 @@
-from sqlite import Sqlite
+import ConfigParser
 
-
-
-
-sqlite = Sqlite()
-
-sqlite.open("test.db")
-
-sqlite.execute("create table if not exists projects(id integer primary key autoincrement, project_name varchar(128))")
-result = sqlite.execute("select * from projects")
-for record in result:
-	for field in record:
-		print field
-
-name = "test"
-sqlite.execute("insert into projects(project_name) values('%s')"%(name))
-
-sqlite.close()
-
-
-
-
-
-
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+result = config.get("test","test")
+print result
